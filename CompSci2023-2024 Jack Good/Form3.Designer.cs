b25f9a6e854c6921b87e8dc5378cd7002301273f
WantedBytheFBI <jackmichaelgood@gmail.com>
2023-11-20 12:14:08 -0500

    QuickSort to be implemented

---
+++ CompSci2023-2024 Jack Good/Form3.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CompSci2023_2024_Jack_Good
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Generate100Shapesbutton = new System.Windows.Forms.Button();
+            this.BubbleSortButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.QuickSortButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Generate100Shapesbutton
+            // 
+            this.Generate100Shapesbutton.Location = new System.Drawing.Point(12, 5);
+            this.Generate100Shapesbutton.Name = "Generate100Shapesbutton";
+            this.Generate100Shapesbutton.Size = new System.Drawing.Size(135, 29);
+            this.Generate100Shapesbutton.TabIndex = 0;
+            this.Generate100Shapesbutton.Text = "Generate Shapes";
+            this.Generate100Shapesbutton.UseVisualStyleBackColor = true;
+            this.Generate100Shapesbutton.Click += new System.EventHandler(this.Generate100Shapesbutton_Click);
+            // 
+            // BubbleSortButton
+            // 
+            this.BubbleSortButton.Location = new System.Drawing.Point(153, 5);
+            this.BubbleSortButton.Name = "BubbleSortButton";
+            this.BubbleSortButton.Size = new System.Drawing.Size(118, 29);
+            this.BubbleSortButton.TabIndex = 1;
+            this.BubbleSortButton.Text = "Bubble Sort";
+            this.BubbleSortButton.UseVisualStyleBackColor = true;
+            this.BubbleSortButton.Click += new System.EventHandler(this.BubbleSortButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(289, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(245, 404);
+            this.listBox1.TabIndex = 2;
+            // 
+            // QuickSortButton
+            // 
+            this.QuickSortButton.Location = new System.Drawing.Point(12, 40);
+            this.QuickSortButton.Name = "QuickSortButton";
+            this.QuickSortButton.Size = new System.Drawing.Size(135, 29);
+            this.QuickSortButton.TabIndex = 3;
+            this.QuickSortButton.Text = "Quick Sort";
+            this.QuickSortButton.UseVisualStyleBackColor = true;
+            this.QuickSortButton.Click += new System.EventHandler(this.QuickSortButton_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.QuickSortButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.BubbleSortButton);
+            this.Controls.Add(this.Generate100Shapesbutton);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button Generate100Shapesbutton;
+        private Button BubbleSortButton;
+        private ListBox listBox1;
+        private Button QuickSortButton;
+    }
+}
---
+++ CompSci2023-2024 Jack Good/Form3.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompSci2023_2024_Jack_Good
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        Shapes[] HundredShapes = new Shapes[100];
+
+        public Shapes[] QuickSort(Shapes[] GettingSorted)
+        {
+
+            return (GettingSorted);
+        }
+        private void Generate100Shapesbutton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Random MakeShape = new Random();
+                switch(MakeShape.Next(0,3))
+                {
+                    case 0:
+                        HundredShapes[i] = new Circles(MakeShape.Next(1, 25));
+                        break;
+                    case 1:
+                        HundredShapes[i] = new Ellipse(MakeShape.Next(1, 25), MakeShape.Next(1, 25));
+                        break;
+                    case 2:
+                        HundredShapes[i] = new Rectangles(MakeShape.Next(1, 25), MakeShape.Next(1, 25));
+                        break;
+                }
+            }
+        }
+
+        private void BubbleSortButton_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 100;i++)
+            {
+                for(int b = 0; b < 100-i-1; b++)
+                {
+                    if (HundredShapes[b].Area > HundredShapes[b + 1].Area)
+                    {
+                        Shapes temp = HundredShapes[b];
+                        HundredShapes[b] = HundredShapes[b + 1];
+                        HundredShapes[(b+1)] = temp;
+                    }
+                }
+            }
+            listBox1.Items.Clear(); //clears the items in this listbox
+            foreach (Shapes NamelessShape in HundredShapes)
+            {
+                string ShapeArea = "";
+                ShapeArea += NamelessShape.ToString();
+                ShapeArea += " with Area: " + NamelessShape.Area;
+                listBox1.Items.Add(ShapeArea);
+            }
+        }
+
+        private void QuickSortButton_Click(object sender, EventArgs e)
+        {
+            HundredShapes = QuickSort(HundredShapes);
+            foreach (Shapes NamelessShape in HundredShapes)
+            {
+                string ShapeArea = "";
+                ShapeArea += NamelessShape.ToString();
+                ShapeArea += " with Area: " + NamelessShape.Area;
+                listBox1.Items.Add(ShapeArea);
+            }
+        }
+    }
+}
